4d99b7d9997ce70f5aee2203cec06cc3400e4b63
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2023-09-30 03:20:08 +0200

    Shooter
    Simple shooter to shoot the Pinball

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
     public float minPower;
     [SerializeField] float power;
-    [SerializeField] float maxPower;
+    public float maxPower;
+
+    [SerializeField] bool bPowerUp;
+    [SerializeField] bool bPinballHere;
+
+    [SerializeField] GameObject pinball;    
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +23,39 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            bPowerUp = true;
+
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+            releasePower();
+
+        if (bPowerUp && power < maxPower)
+            power += Time.deltaTime*50;
+    }
+
+    void releasePower()
+    {
+        if (bPinballHere)
+            pinball.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, power), ForceMode.VelocityChange);
+        power = 0;
+        bPowerUp = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Pinball")
+        {
+            pinball = other.GameObject();
+            bPinballHere = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Pinball")
+        {
+            pinball = null;
+            bPinballHere = false;
+        }
     }
 }
